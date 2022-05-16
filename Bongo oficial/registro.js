fd47bfe0b6f6f7d4033c78acf5c756f8ea89858e
yonxfree <105681517+yonxfree@users.noreply.github.com>
2022-05-16 18:19:34 -0500

    initial commit

---
+++ Bongo oficial/registro.js
@@ -0,0 +1,13 @@
+function ir(){
+
+    var c=2005;
+    var u="NAME";
+    if(document.form.password.value==c && document.form.login.value==u){
+        alert ("Bienvenido a Bongo Eventos")
+        Window.location="Bongo Eventos Oficial.html";}
+    
+
+    else{
+        alert("Porfabor ingrese los datos correctos.");
+    }
+}
